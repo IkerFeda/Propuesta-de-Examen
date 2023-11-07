c62c7cb02a98d977903f51822f54615f7f29eeb9
Iker Fernandez Dacasa <iferndac@myuax.com>
2023-11-07 11:41:49 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <cmath>
 
-int main() {
-    int limit = 30;
-    for (int a = 1; a <= limit; a++) {
-        for (int b = a; b <= limit; b++) {
-            int c_square = a * a + b * b;
-            int c = sqrt(c_square);
-            if (c * c == c_square && c <= limit) {
+int main (){
+    int limite = 30;
+    for (int a = 1; a <= limite; a++){
+        for (int b = a; b <= limite; b++){
+            int c_cuadrada = (a * a + b * b);
+            int c = sqrt(c_cuadrada);
+            if (c * c == c_cuadrada && c <= limite){
                 std::cout << a << " - " << b << " - " << c << std::endl;
             }
         }
     }
-    return 0;
 }
