8be92f736c52ccfb447bd7813f063adf87b17d6a
Iker Fernandez Dacasa <iferndac@myuax.com>
2023-11-07 11:32:37 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 
 int main (){
     int a,b,c, limite;
-    limite = 10;
-    for (a = 1; a <= limite; a++){
+    limite = 30;
+
+    for (a = 3; a <= limite; a++){
+
        for (b = a; b <= limite; b++){
            c = sqrt(a^2 + b^2 );
-           if ( c^2 == a^2 + b^2 && (a + b > c) && (b + c > a) && (a + c > b)&& c<=limite) {
+
+           if (c <= limite && c * c == (a * a + b * b) && a + b + c == 2 * limite) {
                std::cout << a << " - " << b << " - " << c << std::endl;
            }
        }
